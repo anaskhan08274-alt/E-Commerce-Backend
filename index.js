eb3fac621dbad6b5dd866af00b00d27e950f1508
anaskhan08274-alt <anaskhan08274@gmail.com>
2026-02-28 16:21:12 +0530

    letest-commits

--- index.js
+++ index.js
@@ -17,4 +17,10 @@ app.get("/", (req, res) => {
   res.send("Backend is Running...");
 });
 
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`server is runing in the port ${PORT}`)
+})
+
 module.exports = app;
