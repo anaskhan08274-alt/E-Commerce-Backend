8ea419fe8d36e25bf14c4c00bbed3cfcd566460c
anaskhan08274-alt <anaskhan08274@gmail.com>
2026-02-28 15:22:03 +0530

    letest-commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const connectDB = require("../config/db");
+const connectDB = require("./config/db");
 
 dotenv.config();
 connectDB();
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api", require("../route/userRoutes"));
+app.use("/api", require("./route/userRoutes"));
 
 app.get("/", (req, res) => {
   res.send("Backend is Running...");
